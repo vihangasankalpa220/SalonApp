3d4d8e99037287b8b628bb16b6384684400fda63
Vihanga <vihangasankalpa220@gmail.com>
2023-08-27 22:27:14 +0530

    Modified

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         elevation: 0,
-            title: const Text("It's Nail day"),
+            title: const Text("Kandy"),
         foregroundColor : Colors.black,
         centerTitle: false,
         leading: IconButton(
@@ -28,6 +28,7 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {}),
         actions: <Widget>[
           Container(
+
             decoration: BoxDecoration(),
             margin: EdgeInsets.only(right: 15, top: 15),
             child: CircleAvatar(
@@ -39,15 +40,26 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Container(
+      body:
+      Container(
         width: screen_size_width,
         height: screen_size_height,
         padding: EdgeInsets.only(top:16, left:16, right: 16),
+
         child: SingleChildScrollView(
+
+
+
           child: Column(
             children: <Widget>[
 
-              SizedBox(height: 15),
+              Row(
+          children: <Widget>[ Text("Today's Top Picks",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w600))],),
+              SizedBox(height: 5),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.max,
@@ -149,6 +161,128 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               SizedBox(height: 6),
+
+
+
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                        child: Text("Ongoing Appointments",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600))),
+                    FlatButton(
+                        onPressed: () {},
+                        child: Text("View All",
+                            style: TextStyle(color: Colors.black54)))
+                  ]),
+
+              SizedBox(height: 6),
+
+
+              Container(
+                  height: screen_size_height * .3,
+                  width: MediaQuery.of(context).size.width * 50,
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+
+                         Container(
+                           width: MediaQuery.of(context).size.width * .5,
+                          height: 200,
+                          padding: new EdgeInsets.all(10.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            color: Colors.white70,
+                            elevation: 10,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                const ListTile(
+                                  leading:
+                                  CircleAvatar(
+                                    radius: 22,
+                                    backgroundImage: AssetImage(
+                                      "images/profile.jpg",
+                                    ),
+                                  ),
+
+
+
+
+                                  title: Text(
+                                      'Full colour red patten design',
+                                      style: TextStyle(fontSize: 50.0)
+                                  ),
+                                  subtitle: Text(
+                                      'The red pattern in kandy\n12/12/2024 AT 9.30 AM',
+
+                                      style: TextStyle(fontSize: 18.0,color: Colors.black12)
+                                  ),
+
+                                  trailing: Text(
+                                      'Rs: 6000.00',
+
+                                      style: TextStyle(fontSize: 18.0,color: Colors.blue)
+                                  ),
+
+                                ),
+
+                     Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+
+                                ElevatedButton(
+                                  child: Text("Pending"),
+                                  onPressed: () => print("it's pressed"),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.orange,
+                                    onPrimary: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(32.0),
+                                    ),
+                                  ),
+                                ),
+
+                      ],),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                    ],
+                  )),
+
+
+
+
+              ListTile(
+                title: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                          width: MediaQuery.of(context).size.width * .5,
+                          height: 40,
+                          child: RaisedButton(onPressed: () {Navigator.pushNamed(context, UIData.loginPageRoute);},
+                            child: Text("MAKE YOUR APPOINTMENT TODAY ->"),color: Colors.lightBlueAccent,textColor: Colors.black,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+
+                          )
+
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+
+
               Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
